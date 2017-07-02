cee4f9985e3394448223831cec8ce9f25665c9cb
josh gordonson <muffin@mit.edu>
2017-07-02 18:21:43 -0400

    adding new code to handle capacitive touch data and also enable muticiever mode - should have commited the capacitive touch data sketch first, but oh well!

--- arduino/rawdata-rx/rawdata-rx.ino
+++ arduino/rawdata-rx/rawdata-rx.ino
@@ -1,35 +1,39 @@
 #include <SPI.h>
 #include <nrf.h>
 
-// set pin 10 as the slave select for the digital pot:
-const int ssp = 5; //CSN
-const int CE = 6; //CE
+
+#define VERSION 1
+#define CHANNEL 2
+#define BOARD 0
+
+//    OLD
+#define ssp  6 //CSN
+#define CE  5 //CE
+#ifdef VERSION
+//    NEW
+#define ssp 17 //CSN
+#define CE 11 //CE
+#endif
+
+
+
 uint8_t blah[32];
 uint8_t datarx[32];
 float refloat;
-int tempx;
-int tempy;
-int tempz;
-int tempw;
-int ox;
-int oy;
-int oz;
-int ax;
-int ay;
-int az;
-int gx;
-int gy;
-int gz;
-int mx;
-int my;
-int mz;
-int sc;
-int gc;
-int ac;
-int mc;
+int tempx, tempy, tempz, tempw;
+int ox, oy, oz;
+int ax, ay, az;
+int gx, gy, gz;
+int mx, my, mz;
+int sc, gc, ac, mc;
+int t0, t1, t2, t3;
 uint8_t rxcount;
+uint8_t rxaddr[5] = {0xA5, 0xA5, 0xA5, 0xA5, 0xA5};
 
 void setup() {
+  while(!Serial);
+  Serial.begin(115200);
+  Serial.print("ORIENTATION: ");
   datarx[0]=0;
   for(uint8_t i=0;i<32;i++){
     blah[i]=i;
@@ -45,49 +49,82 @@ void setup() {
   SPI.begin();
   SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
   SPI.endTransaction();
-  Serial.begin(115200);
 
-  nrfWrite(EN_AA,(1<<ENAA_P0));  // auto ack pipe0
-  nrfWrite(EN_RXADDR,(1<<ERX_P0));  //enable data pipe 0
+  nrfWrite(EN_AA,(1<<ENAA_P0)|(1<<ENAA_P1));  // auto ack pipe0,1
+  nrfWrite(EN_RXADDR,(1<<ERX_P0)|(1<<ERX_P1));  //enable data pipe 0,1
   nrfWrite(SETUP_AW,(3<<AW));  //5 bite address width
   nrfWrite(SETUP_RETR,((15)<<ARD)|(5<<ARC));  //default 250uS retx delay, 3x retx
-  nrfWrite(RF_CH,2);  //channel 2
-  nrfWrite(RF_SETUP,(1<<RF_DR_LOW)|(0<<RF_PWR)); //low data rate, low power (0b100000)
+  
+  //SET RX ADDRESSES
+  rxaddr[0] = 0xA5;
+  nrfSetRxAddr(rxaddr,5,0);
+  rxaddr[0] = 0xC3;
+  nrfSetRxAddr(rxaddr,5,1);
+  rxaddr[0] = 0x92;
+  nrfSetRxAddr(rxaddr,5,2);
+  rxaddr[0] = 0x0F;
+  nrfSetRxAddr(rxaddr,5,3);
+  rxaddr[0] = 0x4D;
+  nrfSetRxAddr(rxaddr,5,4);
+  rxaddr[0] = 0xB7;
+  nrfSetRxAddr(rxaddr,5,5);
+  
+  //SET CHANNEL
+  nrfWrite(RF_CH,CHANNEL); 
+  nrfWrite(RF_SETUP,(1<<RF_DR_LOW)|(1<<RF_PWR_LOW)|(1<<RF_PWR_HIGH)); //low data rate, low power (0b100000)
   //nrfWrite(DYNPD,(1<<DPL_P0)); //enable dynamic payload data pipe 0
   //nrfWrite(FEATURE,(1<<EN_DPL)|(1<<EN_ACK_PAY)); //enable dynamic payload with ack
   
   nrfWrite(NRF_CONFIG,(1<<EN_CRC)|(1<<CRCO)|(1<<PWR_UP)|(1<<PRIM_RX));  //power on, PRX, 2-byte CRC
   nrfWrite(NRF_STATUS,(1<<RX_DR));  //clear rx_dr interrupt flag
   nrfWrite(FLUSH_RX,0); //clear rx fifo
-  nrfRead(6,1); 
-  nrfWrite(7,0x40);
-  nrfWrite(0x11,10);
-  nrfRead(0x17,1);
-  nrfRead(0,1);
+  nrfRead(RF_SETUP,1); 
+  nrfWrite(7,(1<<RX_DR));
+  
+  //SET DATA PACKET LENGTH
+  nrfWrite(RX_PW_P0,10);
+  nrfWrite(RX_PW_P1,10);
+  nrfWrite(RX_PW_P2,10);
+  nrfWrite(RX_PW_P3,10);
+  nrfWrite(RX_PW_P4,10);
+  nrfWrite(RX_PW_P5,10);
+  
+  nrfRead(FIFO_STATUS,1);
+  nrfRead(NRF_CONFIG,(1<<PRIM_RX));
   digitalWrite(CE, HIGH);
+  
 }
 
 void loop() {
-  delay(10);
+  
+  //delay(10);
   if(((nrfReadq(NRF_STATUS,1)[0]&(1<<RX_DR))==(1<<RX_DR))||
      ((nrfReadq(FIFO_STATUS,1)[0]&0x02)==0x02) ||
      ((nrfReadq(FIFO_STATUS,1)[0]&0x01)==0x01)){
+       
       if(((nrfReadq(FIFO_STATUS,1)[0]&0x02)==0x02)){
-        //Serial.println("FIFO FUL");
-      }
+        Serial.println("FIFO FUL");
+        //Serial.println(a);
+        nrfWrite(FLUSH_RX,0);
+      }    
+    
     uint8_t a = nrfReadq(R_RX_PL_WID,1)[0];
-   // Serial.println(a);
+    //Serial.print(a);
+    //Serial.println(a);
     if(a > 32) {  
       nrfWrite(FLUSH_RX,0);
-    }
-    
+    }else{
+    //Serial.print(a);
     //delay(10);
     nrfReadq(0x61,a);
+    }
+    
     tempx = (datarx[2]<<8)+datarx[3];
     tempy = (datarx[4]<<8)+datarx[5];
     tempz = (datarx[6]<<8)+datarx[7];
     tempw = (datarx[8]<<8)+datarx[9];
-    if(datarx[1]<=16){
+    
+    if(datarx[1]<=32){
       if(datarx[1]==2){ 
         ax = tempx;
         ay = tempy;
@@ -141,16 +178,20 @@ void loop() {
           rxcount |= 16;
         }       
       }
-      //Serial.print("X: ");
-//      Serial.print(tempx);
-//      Serial.print(":");
-//      Serial.print(tempy);
-//      Serial.print(":");
-//      Serial.print(tempz);
-//      Serial.print("/");
+      if(datarx[1]==32){
+        t0 = tempx;
+        t1 = tempy;
+        t2 = tempz;
+        t3 = tempw;
+        if((rxcount&32)==32){
+          rxcount = 32;
+        }else{
+          rxcount |= 32;
+        }       
+      }
     }
     //Serial.println(rxcount);
-    if(rxcount == 31){
+    if((rxcount&0x1f) == 31){
 
       Serial.print("ORIENTATION: ");
       Serial.print(ox, DEC);
@@ -190,13 +231,20 @@ void loop() {
       Serial.print(mc, DEC);
       Serial.println("/");
     }
-   
+    if((rxcount&0x20) == 32){
+      Serial.print("TOUCH: ");
+      Serial.print(t0, DEC);
+      Serial.print(":");
+      Serial.print(t1, DEC);
+      Serial.print(":");
+      Serial.print(t2, DEC);
+      Serial.print(":");
+      Serial.print(t3, DEC);
+      Serial.println("/");
+    }
     //delay(10);
     //byte2Float(datarx);
     //Serial.println(refloat);
-    for(char i=0;i<a;i++){
-      //Serial.print(datarx[i],HEX);
-    }
     //Serial.print("\n");
     //Serial.println(10);
     //Serial.println(dat[10],HEX);
@@ -216,8 +264,9 @@ void loop() {
   }
     //nrfRead(0x17,1);
       if(((nrfReadq(FIFO_STATUS,1)[0]&0x02)==0x02)){
-        //Serial.println("FIFO FULL");
+        Serial.println("FIFO FULL");
       }
+      //Serial.println("fee fi fo fum");
 }
 
 uint8_t *nrfReadq(unsigned char address, unsigned char numBy){
@@ -233,6 +282,15 @@ uint8_t *nrfReadq(unsigned char address, unsigned char numBy){
 void nrfWriteq(unsigned char address, unsigned char numBytes, unsigned char *data){
 
   
+}
+
+void nrfSetRxAddr(uint8_t *data, uint8_t numBytes,uint8_t chan){
+  digitalWrite(ssp, LOW);
+  SPI.transfer((10+chan));
+  for(char i=0;i<numBytes;i++){
+    SPI.transfer(data[i]);
+  }
+  digitalWrite(ssp, HIGH);
 }
 
 char nrfRead(int address, int numBytes) {
